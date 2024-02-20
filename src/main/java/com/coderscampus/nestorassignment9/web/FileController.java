d0016fdb36f42e4ac2d6634c0e98e966043927fe
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-02-20 17:07:18 -0500

    Testing how things work

--- src/main/java/com/coderscampus/nestorassignment9/web/FileController.java
+++ src/main/java/com/coderscampus/nestorassignment9/web/FileController.java
@@ -1,5 +1,13 @@
 package com.coderscampus.nestorassignment9.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FileController {
 
+	@GetMapping("")
+	public String saySomething() {
+		return "Say Something Adam!!!!";
+	}
 }
