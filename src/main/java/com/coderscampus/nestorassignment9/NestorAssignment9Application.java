e5a3b972a9d8ab3086e193a4c5fdd99f65c796a8
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-02-20 16:59:51 -0500

    initial commit

---
+++ src/main/java/com/coderscampus/nestorassignment9/NestorAssignment9Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.nestorassignment9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestorAssignment9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestorAssignment9Application.class, args);
+	}
+
+}
