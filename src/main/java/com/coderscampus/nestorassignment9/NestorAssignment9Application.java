a7b796d050d35df667093f30bfaae0f5c53bddea
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-02-20 22:11:14 -0500

    update files

--- src/main/java/com/coderscampus/nestorassignment9/NestorAssignment9Application.java
+++ src/main/java/com/coderscampus/nestorassignment9/NestorAssignment9Application.java
@@ -1,13 +1,15 @@
 package com.coderscampus.nestorassignment9;
 
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NestorAssignment9Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(NestorAssignment9Application.class, args);
+		
 	}
 
 }
--- src/main/java/com/coderscampus/nestorassignment9/service/FileService.java
+++ src/main/java/com/coderscampus/nestorassignment9/service/FileService.java
@@ -1,7 +1,31 @@
 package com.coderscampus.nestorassignment9.service;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 public class FileService {
 
-	
-	
+	private String fileName;
+
+	public FileService() {
+	}
+
+	public FileService(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public void processCSV() throws IOException {
+		Reader in = new FileReader("recipes.txt");
+		Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+		for (CSVRecord record : records) {
+			record.get(0);
+			record.get(1);
+			record.get(2);
+		}
+	}
+
 }
